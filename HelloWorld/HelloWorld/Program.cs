5ce77a3b3fcd2d7b8e043b13ea8b484c75d10190
unknown <andrbter@gmail.com>
2022-02-23 17:16:06 -0300

    Aulas de C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Variaveis inteiras
+
+            int idade = 18, altura = 95 , sapato = 45;
+
+
+            
+            
+
+            Console.WriteLine("Hello World! His age is " + idade + " years. My heygth is " + altura);
+
+            
+            
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
